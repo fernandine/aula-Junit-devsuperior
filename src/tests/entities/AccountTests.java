1a1be0485966272d5f882650e29590a649636788
Jean Fernandine <jfernandine@gmail.com>
2022-05-20 15:31:10 -0300

    aula JUnit

---
+++ src/tests/entities/AccountTests.java
@@ -0,0 +1,25 @@
+package tests.entities;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import entities.Account;
+
+public class AccountTests {
+	
+	@Test
+	public void depositShouldIncreaseBalanceWhenPositiveAmount() {
+		
+		//Arrange
+		double amount = 200.0;
+		double expectedValue = 196.0;
+		Account acc = new Account(1L, 0.0);
+		
+		//Act
+		acc.deposit(amount);
+	
+		//Assert
+		Assertions.assertEquals(expectedValue, acc.getBalance());
+	}
+
+}
